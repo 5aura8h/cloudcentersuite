93bf317ec684d0c33558c87d881886c3c9d6a13b
5aura8h <44212751+5aura8h@users.noreply.github.com>
2020-06-10 18:20:36 +0530

    Update wp-restore.sh

--- apps/wordpress/wp-restore.sh
+++ apps/wordpress/wp-restore.sh
@@ -26,8 +26,8 @@ sudo echo "aws_secret_access_key=$aws_secret_access_key" | sudo tee --append /ro
 sudo cp /var/www/wp-config.php /tmp
 sudo rm -rf /var/www/*
 
-sudo /root/bin/aws s3 cp s3://$s3path/$migrateFromDepId/wordpressbkup.zip ~/wordpressbkup.zip
-sudo unzip -o ~/wordpressbkup.zip -d /var/www/
+sudo /root/bin/aws s3 cp s3://$s3path/$migrateFromDepId/wordpressbkup.zip /tmp/wordpressbkup.zip
+sudo unzip -o /tmp/wordpressbkup.zip -d /var/www/
 sudo cp /tmp/wp-config.php /var/www/
 sudo chown -R apache:apache /var/www/
 
